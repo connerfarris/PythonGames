c71d26da36fc141f3e0b7d0f92d10d6ce22b4252
cfarris <connerfarris@gmail.com>
2020-02-13 16:51:42 -0700

    refactor tiling

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,23 +1,27 @@
-board = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
-player = 'X'
-
-
 def printBoard(board):
 	for row in board:
 		print(row[0] + ' | ' + row[1] + ' | ' + row[2])
 
 
 def play(board, player):
-	xCoordinate = int(input('x coordinate? '))
-	yCoordinate = int(input('y coordinate? '))
-	while (xCoordinate not in {0,1,2} or yCoordinate not in {0,1,2} or board[yCoordinate][xCoordinate] != '_' ):
+	tileMap = {1: [0,0], 2: [1, 0], 3: [2, 0], 4: [0, 1], 5: [1, 1], 6: [1, 2], 7: [2, 0], 8: [2, 1], 9: [2, 2]}
+	tile = int(input('which tile will you play on? '))
+	while (tile not in {1,2,3,4,5,6,7,8,9}):
 		print('invalid move, try again')
-		xCoordinate = int(input('x coordinate? '))
-		yCoordinate = int(input('y coordinate? '))
-	board[yCoordinate][xCoordinate] = player
+		tile = int(input('which tile will you play on? '))
+	board[tileMap[tile][1]][tileMap[tile][0]] = player
+
+
+def draw(board):
+	for row in board:
+		if (row.count('_') > 0):
+			return False
+	return True
 
 
 def gameOn(board):
+	if (draw(board)):
+		return False
 	winningLines = [[[0, 0], [0, 1], [0, 2]], [[1, 0], [1, 1], [1, 2]], [[2, 0], [2, 1], [2, 2]],
 					[[0, 0], [1, 1], [2, 2]], [[2, 0], [1, 1], [0, 2]]]
 	xAndO = set()
@@ -31,18 +35,23 @@ def gameOn(board):
 	return True
 
 
-while (gameOn(board) == True):
-	play(board, player)
+def continuousGame():
+	board = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+	player = 'X'
+	while (gameOn(board) == True):
+		play(board, player)
+		if (player == 'X'):
+			player = 'O'
+		else:
+			player = 'X'
+		print()
+		printBoard(board)
+		print()
+
 	if (player == 'X'):
-		player = 'O'
+		print('\nCongrats O!')
 	else:
-		player = 'X'
-	print()
-	printBoard(board)
-	print()
+		print('\nCongrats X!')
 
-if (player == 'X'):
-	print('\nCongrats O!')
-else:
-	print('\nCongrats X!')
 
+continuousGame()
